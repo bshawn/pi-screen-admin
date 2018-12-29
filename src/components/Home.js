8d37f54f3fc80d4a146e2278ac2e347978f5b1ea
bshawn <bret.shawn@gmail.com>
2018-12-29 10:21:22 -0500

    Add home screen content

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,30 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <h1>Home</h1>
+      <div class="container">
+        <section class="hero is-medium is-primary is-bold">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                FUMC Digital Signage
+            </h1>
+              <h2 class="subtitle">
+                Salem, VA
+            </h2>
+            </div>
+          </div>
+        </section>
+        <body>
+          <section class="section">
+            <div class="container">
+              <h1 class="title">Welcome</h1>
+              <h2 class="subtitle">
+                Welcome to the Digital Signage <strong>Administrator Portal</strong>
+              </h2>
+            </div>
+          </section>
+        </body>
+      </div>
     );
   }
 }
