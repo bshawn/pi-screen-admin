69af22a3989b6e93beceb43fef5bbc4f014fb105
bshawn <bret.shawn@gmail.com>
2019-01-01 20:32:25 -0500

    Screens reactive layout

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -3,7 +3,26 @@ import React, { Component } from 'react';
 class Screens extends Component {
   render() {
     return (
-      <h1>Screens</h1>
+      // <div class="container">
+      //   <body>
+      //     <section class="section">
+      //       <div class="container">
+      //         <h1 class="title">Screens</h1>
+      //       </div>
+      //     </section>
+      //   </body>
+      // </div>
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile">
+          <h1 class="title">List 3col</h1>
+        </div>
+        <div class="column is-hidden-mobile">
+          <h1 class="title">Content</h1>
+        </div>
+        <div class="column is-hidden-tablet">
+          <h1 class="title">List</h1>
+        </div>
+      </div>
     );
   }
 }
